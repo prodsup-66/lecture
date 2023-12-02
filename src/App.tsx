8c5498cd0bdca3b5cb63e46c3f95dca86104246e
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-12-03 06:57:37 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import useData from "./lib/useData";
 
 const youtubeURL803 =
   "https://youtube.com/playlist?list=PLNGLpHQhvGrt3R8-qP3sbTfZvD9Ye6psv&si=HjpoGc7Q1PPYljuY";
+const pastProjectURL =
+  "https://youtube.com/playlist?list=PLNGLpHQhvGrtqn6UYxUYXm7C0auAYrh_O&si=GxlTb1B8mTPiG9f3";
+
 function App() {
   useData();
   return (
@@ -11,13 +14,23 @@ function App() {
         <div className="font-extrabold text-transparent text-6xl lg:text-8xl bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
           Prodsup 66
         </div>
-        <a
-          href={youtubeURL803}
-          target="_blank"
-          className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
-        >
-          Youtube VDO (003/803)
-        </a>
+
+        <div className="flex gap-2">
+          <a
+            href={youtubeURL803}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
+          >
+            Youtube VDO (003/803)
+          </a>
+          <a
+            href={pastProjectURL}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-purple-500 text-white text-sm lg:text-lg font-bold"
+          >
+            Projects
+          </a>
+        </div>
       </div>
       <div className="mt-8">
         <FileBrowser />
