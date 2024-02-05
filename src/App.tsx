ea332e504983427d651b3dd3192ea6c2c2debf71
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-02-05 09:43:19 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ const ytURL_003 =
   "https://youtube.com/playlist?list=PLNGLpHQhvGrt3R8-qP3sbTfZvD9Ye6psv&si=HjpoGc7Q1PPYljuY";
 const ytURL_004 =
   "https://youtube.com/playlist?list=PLNGLpHQhvGruI5ZjgURM4TOsVJMB1RV-N&si=AL5Mud7HZ3q2GdF2";
+const ytURL_002 =
+  "https://youtube.com/playlist?list=PLNGLpHQhvGrthxX356tY7muwv-irYz65n&si=Z6zAd_5U20rFkgl5";
+const ytURL_006 =
+  "https://youtube.com/playlist?list=PLNGLpHQhvGrvTx3yNAW2qsFQW5hbXGykL&si=q_XAXfwa5PV6n6j6";
 const pastProjectURL =
   "https://youtube.com/playlist?list=PLNGLpHQhvGrtqn6UYxUYXm7C0auAYrh_O&si=GxlTb1B8mTPiG9f3";
 
@@ -28,6 +32,13 @@ function App() {
             YT (001/801)
           </a>
           <a
+            href={ytURL_002}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
+          >
+            YT (002/802)
+          </a>
+          <a
             href={ytURL_003}
             target="_blank"
             className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
@@ -40,6 +51,13 @@ function App() {
             className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
           >
             YT (004/804)
+          </a>
+          <a
+            href={ytURL_006}
+            target="_blank"
+            className="border px-2 py-1 rounded-lg bg-red-600 text-white text-sm lg:text-lg font-bold"
+          >
+            YT (006/805)
           </a>
           <a
             href={pastProjectURL}
